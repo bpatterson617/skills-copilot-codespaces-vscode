105984976cb87b6366fb473d01b54b748dcc64bb
bpatterson617 <56075893+bpatterson617@users.noreply.github.com>
2024-09-17 16:56:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = new Member();
+    member.skills = [
+        { name: 'JavaScript', level: 5 },
+        { name: 'HTML', level: 4 },
+        { name: 'CSS', level: 3 }
+    ];
+    return member;
+}
